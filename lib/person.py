0803c536a105b8151d0493b9888643dca27125d4
kinya1738 <shirleen.njeru@student.moringaschool.com>
2025-05-15 04:10:25 -0700

    passing code

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+# lib/person.py
 
-    #Instance method definition
-    pass
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+
+p1= Person()
+print(p1.talk)
+p2= Person()
+print(p2.walk)        
